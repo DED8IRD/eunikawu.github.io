39c936eb5ddf7256d18d7b3928e9a03a31fe3e48
DED8IRD <ewu72829@gmail.com>
2019-04-29 15:12:24 -0700

    fix modal

--- static/js/script.js
+++ static/js/script.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
     $(document).on('click', '.portfolio-container', function(event) {
         const $lightbox = $("<div id='lightbox'></div>");
         const $close = $("<p class='close'>&times;</p>");
-        const $contents = $("<div class='modal-dialogue modal-content'></div>");
+        const $contents = $("<div class='modal-dialogue modal-dialogue-centered modal-content'></div>");
         const $caption = $("<div class='caption-container'></div>");
         const $img = $(this).find('img')[0].outerHTML;
         $caption
