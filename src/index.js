d133839acc88abff0b92d0e65e8b432e77906a13
DED8IRD <ewu72829@gmail.com>
2019-08-24 12:58:02 -0700

    optimize images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import ScrollReveal from 'scrollreveal'
 
 import style from './scss/style.scss'
 
+// import all static images
+import portrait from '../static/images/portrait.png'
+import DAVE from '../static/images/projects/DAVE.png'
+import EchoVisualization from '../static/images/projects/EchoVisualization.png'
+import EmVibe from '../static/images/projects/EmVibe.png'
+import NodingBat from '../static/images/projects/NodingBat.png'
+import ProcessDashboard from '../static/images/projects/ProcessDashboard.png'
+import SENTIENCE from '../static/images/projects/SENTIENCE.png'
+
 $(function() {
     // Preloader animation
     $('.loading').fadeOut('slow', function() {
