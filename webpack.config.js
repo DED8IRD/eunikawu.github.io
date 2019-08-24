4105ad449b0413948a881d6ef1c60cec8a6d3e74
DED8IRD <ewu72829@gmail.com>
2019-08-24 13:48:46 -0700

    optimized progressive images

--- webpack.config.js
+++ webpack.config.js
@@ -64,24 +64,18 @@ module.exports = {
                         loader: 'image-webpack-loader',
                         options: {
                             mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
+                                quality: 65,
+                                progressive: true
                             },
                             pngquant: {
                                 quality: '65-90',
                                 speed: 4
                             },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
+                            // // the webp option will enable WEBP
+                            // webp: {
+                            //     quality: 75
+                            // },
+                            enforce: 'pre'
                         }
                     }
                 ]
